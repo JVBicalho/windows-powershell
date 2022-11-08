80f230ceddabcb82482ba508f5af243a28921cad
JV Bicalho <contatojvbicalho@gmail.com>
2022-11-07 21:25:20 -0300

    Adcionando Textos/estrutura

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import Prompt from './components/Prompt.vue';
 </script>
 
 <template>
-
+  <Prompt/>
 </template>
 
 <style scoped>
---
+++ src/components/Prompt.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+  mounted() {
+    this.$refs.input.focus()
+  }
+}
+</script>
+
+
+<template>
+  <div class="prompt">
+    <div class="header">
+        <div class="abas">
+            <div class="aba">
+                <span>icon</span>Windows PowerShell<span>close</span>
+            </div>
+            <div class="abas-icons">
+                <span>plus</span>
+                <span>arrow-down</span>
+            </div>
+        </div>
+        <div class="icones">
+            <span>-</span>
+            <span>square</span>
+            <span>close</span>   
+        </div>
+    </div>
+    <div class="alerta">
+        <span>!</span>
+        O Terminal do Windows pode ser definido como o aplicativo de terminal padrao em suas configuracoes. <span> Abrir Configuracoes</span>
+        <span>close</span>
+    </div>
+    <div class="input-area">
+        O Windows PowerShell <br>
+        Copyright (C) Microsoft Corporation. Todos os direitos reservados.<br>
+
+        Instale o PowerShell mais recente para obter novos recursos e aprimoramentos! https://aka.ms/PSWindows <br>
+        <br>
+        <br>
+        PS C:\Users\conta\Desktop>
+    </div>
+  </div>
+</template>
+<style scoped>
+.prompt{
+    background-color: black;
+    color: #fff;
+    
+
+    
+}
+.header{
+    background-color: aqua;
+    padding: 1vw ;
+    display: flex;
+    flex-direction: row;
+
+}
+.alerta{
+    padding: 2vw;
+}
+.input-area{
+    font-family: "Cascadia Mono";
+    color: #CCCCCC;
+    padding: 1vw ;
+}
+
+</style>
